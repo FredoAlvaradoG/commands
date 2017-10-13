e65c081268bc89af1261f2a9633b6b2212a7f172
Fredo Alvarado G <alfred.aag@gmail.com>
2017-10-12 22:44:58 -0500

    Upgrade firts And Second commands created

--- firstcommands.js
+++ firstcommands.js
@@ -1,21 +1,13 @@
 git init
 
-git add filename
-
-git add .
+git add filename / git add .
 
 git status
 
-git rm --cached filename
+git rm --cached filename / git rm --cached -r .
 
-git rm --cached -r .
+git commit -m "Description"  / git commit -a -m "addAndCommit"
 
-git commit -m "Description"
+git log / git log --online
 
-git log
-
-git log --online
-
-git checkout ID
-
-git checkout master
+git checkout ID / git checkout master
---
+++ secondcommands.js
@@ -0,0 +1,11 @@
+git clone URL
+
+git checkout -b crazy-idea
+
+git branch
+
+git push --set-upstream origin crazy-idea
+git checkout master
+git pull
+
+git branch --delete crazy-idea
