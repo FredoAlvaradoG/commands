37b21ff2e5b16bc514393ce25db9594175c837ae
Fredo Alvarado G <alfred.aag@gmail.com>
2017-10-12 21:45:55 -0500

    BasicCommands

---
+++ firstcommands.js
@@ -0,0 +1,21 @@
+git init
+
+git add filename
+
+git add .
+
+git status
+
+git rm --cached filename
+
+git rm --cached -r .
+
+git commit -m "Description"
+
+git log
+
+git log --online
+
+git checkout ID
+
+git checkout master
