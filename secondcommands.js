841bcc01496e327bdfab7023d047649aae6d5049
FredoAlvaradoG <alfred.aag@gmail.com>
2017-10-18 17:05:51 -0600

    Common Workflow

--- secondcommands.js
+++ secondcommands.js
@@ -1,11 +1,14 @@
 git clone <URL> <where to clone>   //Clone Repository into a new Directory
-
 git clone <LocalRepo> <where to clone>   //Clone Repository into a new Directory
 
-git checkout -b crazy-idea
 
-git branch
-
+//Common Workflow
+git checkout -b crazy-idea //Create the branch and switched to
+git branch // View the branches
+//CommonCommands like git add, status, commit, etc
+git add .
+git status
+git commit -m "Workflow"
 git push --set-upstream origin crazy-idea //push to remote respository
 git checkout master
 git pull    //Pull lastest from remote respository
