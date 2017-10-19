fb311255762f1de7a9b9cbce5f5f5be05229c386
FredoAlvaradoG <alfred.aag@gmail.com>
2017-10-19 14:45:22 -0600

    Comments abotu Git Pages

--- GithubPages.js
+++ GithubPages.js
@@ -2,5 +2,12 @@ make repo with the same name of the user
 make a index.html File. Upload
 access to the web page info via nameUser.github.io
 
+
+//This for repos o organization
 git checkout -b gh-pages
+tocuh index.html
+git add .
+git commit -m "Add index.html"
 git push -u origin gh-pages
+//access via repo: nameUser.github.io/repoName
+//access via organization: organizationName.github.io/repoName
